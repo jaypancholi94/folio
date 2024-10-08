686ae18a3fc837f074dc52824f07df38803bfefc
Jay Pancholi <jaypancholi94@gmail.com>
2024-10-08 21:04:57 +1100

    feat: partytown for GA

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
-
+import partytown from '@astrojs/partytown';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  site: 'https://muggleborn.dev',
+  integrations: [
+    tailwind(),
+    react(),
+    partytown({ config: { forward: ['dataLayer.push'] } }),
+  ],
 });
